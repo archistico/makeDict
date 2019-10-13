4a566fa8a952cd535a128a8c3e871243a6538604
archistico <info@archistico.com>
2019-10-13 23:36:52 +0200

    permutazioni

--- makedict.go
+++ makedict.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 
-func word2Upper(word string) {
+func word2Upper(word string) []string {
 	
+	response := []string {}
+
 	for iMaiuscola := 0; iMaiuscola < len(word); iMaiuscola++ {
 		lista := []string {}
 		
@@ -18,23 +20,49 @@ func word2Upper(word string) {
 			}
 		}
 
-		fmt.Println("Parola maiuscola:", strings.Join(lista,""))
+		response = append(response, strings.Join(lista,""))
 	}
+
+	return response
+}
+
+
+func combina(words []string) []string {
+	
+	response := []string {}
+
+	// aggiungere for che combina un numero di parole da 2 a len(words)
+	
+	for i1Parola := 0; i1Parola < len(words); i1Parola++ {
+		for i2Parola := 0; i2Parola < len(words); i2Parola++ {
+			if i1Parola != i2Parola {
+				response = append(response, words[i1Parola]+words[i2Parola])
+			}
+		}
+	}
+
+	return response
 }
 
 func main() {
 	var words = []string{}
-	words = append(words, "emi")
-	//words = append(words, "rollandin")
+	words = append(words, strings.ToLower("cane"))
+	words = append(words, strings.ToLower("gatto"))
+	words = append(words, strings.ToLower("topo"))
 
+	// COMBINAZIONI
+	if len(words) > 0 {
+		words = append(words, combina(words)...)
+	}
+
+	// MAIUSCOLI
 	var maiuscoli = []string{}
 	if len(words) > 0 {
 		for _, w := range words {
-			word2Upper(w)
+			maiuscoli = append(maiuscoli, word2Upper(w)...)
 		}
 	}
-	words = append(words, maiuscoli...)
-
+	//words = append(words, maiuscoli...)
 
 	// VISUALIZZA 
 
