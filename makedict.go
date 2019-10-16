e9cf12796990c6b2c1b8ce794773265fb3035063
unknown <info@archistico.com>
2019-10-16 18:12:22 +0200

    5 words max

--- makedict.go
+++ makedict.go
@@ -5,38 +5,72 @@ import (
 	"strings"
 )
 
-func word2Upper(word string) []string {
-	
-	response := []string {}
-
-	for iMaiuscola := 0; iMaiuscola < len(word); iMaiuscola++ {
-		lista := []string {}
-		
-		for iCarattere := 0; iCarattere < len(word); iCarattere++ {
-			if iCarattere == iMaiuscola {
-				lista = append(lista, strings.ToUpper(string(word[iCarattere])))
-			} else {
-				lista = append(lista, string(word[iCarattere]))
-			}
-		}
-
-		response = append(response, strings.Join(lista,""))
-	}
-
-	return response
-}
-
-
 func combina(words []string) []string {
 	
 	response := []string {}
 
 	// aggiungere for che combina un numero di parole da 2 a len(words)
 
-	for i1Parola := 0; i1Parola < len(words); i1Parola++ {
-		for i2Parola := 0; i2Parola < len(words); i2Parola++ {
-			if i1Parola != i2Parola {
-				response = append(response, words[i1Parola]+words[i2Parola])
+	if len(words) >= 2 {
+		for i1Parola := 0; i1Parola < len(words); i1Parola++ {
+			for i2Parola := 0; i2Parola < len(words); i2Parola++ {
+				if i1Parola != i2Parola {
+					response = append(response, words[i1Parola]+words[i2Parola])
+				}
+			}
+		}
+	}
+
+	// 3 parole
+	if len(words) >= 3 {
+		for i1Parola := 0; i1Parola < len(words); i1Parola++ {
+			for i2Parola := 0; i2Parola < len(words); i2Parola++ {
+				for i3Parola := 0; i3Parola < len(words); i3Parola++ {
+					if i1Parola != i2Parola && i2Parola != i3Parola && i1Parola != i3Parola  {
+						response = append(response, words[i1Parola]+words[i2Parola]+words[i3Parola])
+					}
+				}
+			}
+		}
+	}
+
+	// 4 parole
+	if len(words) >= 4 {
+		for i1Parola := 0; i1Parola < len(words); i1Parola++ {
+			for i2Parola := 0; i2Parola < len(words); i2Parola++ {
+				for i3Parola := 0; i3Parola < len(words); i3Parola++ {
+					for i4Parola := 0; i4Parola < len(words); i4Parola++ {
+						if i1Parola != i2Parola && i2Parola != i3Parola && i1Parola != i3Parola && i3Parola != i4Parola && i1Parola != i4Parola && i2Parola != i4Parola  {
+							response = append(response, words[i1Parola]+words[i2Parola]+words[i3Parola]+words[i4Parola])
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// 5 parole
+	if len(words) >= 5 {
+		for i1Parola := 0; i1Parola < len(words); i1Parola++ {
+			for i2Parola := 0; i2Parola < len(words); i2Parola++ {
+				for i3Parola := 0; i3Parola < len(words); i3Parola++ {
+					for i4Parola := 0; i4Parola < len(words); i4Parola++ {
+						for i5Parola := 0; i5Parola < len(words); i5Parola++ {
+							if i1Parola != i2Parola &&
+								i2Parola != i3Parola &&
+								i1Parola != i3Parola &&
+								i3Parola != i4Parola &&
+								i1Parola != i4Parola &&
+								i2Parola != i4Parola &&
+								i1Parola != i5Parola &&
+								i2Parola != i5Parola &&
+								i3Parola != i5Parola &&
+								i4Parola != i5Parola {
+								response = append(response, words[i1Parola]+words[i2Parola]+words[i3Parola]+words[i4Parola]+words[i5Parola])
+							}
+						}
+					}
+				}
 			}
 		}
 	}
@@ -44,31 +78,49 @@ func combina(words []string) []string {
 	return response
 }
 
+func word2Title(words []string) []string {
+
+	response := []string {}
+
+	if len(words) > 0 {
+		for _, w := range words {
+			response = append(response, strings.Title(w))
+		}
+	}
+
+	return response
+}
+
+
 func main() {
 	var words = []string{}
-	words = append(words, strings.ToLower("cane"))
-	words = append(words, strings.ToLower("gatto"))
-	words = append(words, strings.ToLower("topo"))
+	var passwords = []string{}
+
+	words = append(words, strings.ToLower("my"))
+	words = append(words, strings.ToLower("secret"))
+	words = append(words, strings.ToLower("number"))
+	words = append(words, strings.ToLower("is"))
+	words = append(words, strings.ToLower("42"))
+
+	// Singoli
+	passwords = append(passwords, words...)
 
 	// COMBINAZIONI
 	if len(words) > 0 {
-		words = append(words, combina(words)...)
+		passwords = append(passwords, combina(words)...)
 	}
 
-	// MAIUSCOLI
-	var maiuscoli = []string{}
 	if len(words) > 0 {
-		for _, w := range words {
-			maiuscoli = append(maiuscoli, word2Upper(w)...)
-		}
+		var wordsTitle = []string{}
+		wordsTitle = append(wordsTitle, word2Title(words)...)
+		passwords = append(passwords, combina(wordsTitle)...)
 	}
-	//words = append(words, maiuscoli...)
 
-	// VISUALIZZA 
+	// VISUALIZZA
 
-	if len(words) > 0 {
-		for _, w := range words {
-			fmt.Println("Word:", w)
+	if len(passwords) > 0 {
+		for _, p := range passwords {
+			fmt.Println("Password:", p)
 		}
 	} else {
 		fmt.Println("Words list is empty")
