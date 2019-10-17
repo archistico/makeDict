f0497cc3b6f0ddfc0ec1505e3973cf6ca808665e
archistico <info@archistico.com>
2019-10-17 10:50:18 +0200

    add export filename

--- makedict.go
+++ makedict.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"crypto/md5"
-	"io"
 	"bufio"
+	"crypto/md5"
+	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func combina(words []string) []string {
-	
-	response := []string {}
+
+	response := []string{}
 
 	// aggiungere for che combina un numero di parole da 2 a len(words)
 
@@ -30,7 +36,7 @@ func combina(words []string) []string {
 		for i1Parola := 0; i1Parola < len(words); i1Parola++ {
 			for i2Parola := 0; i2Parola < len(words); i2Parola++ {
 				for i3Parola := 0; i3Parola < len(words); i3Parola++ {
-					if i1Parola != i2Parola && i2Parola != i3Parola && i1Parola != i3Parola  {
+					if i1Parola != i2Parola && i2Parola != i3Parola && i1Parola != i3Parola {
 						response = append(response, words[i1Parola]+words[i2Parola]+words[i3Parola])
 					}
 				}
@@ -44,7 +50,7 @@ func combina(words []string) []string {
 			for i2Parola := 0; i2Parola < len(words); i2Parola++ {
 				for i3Parola := 0; i3Parola < len(words); i3Parola++ {
 					for i4Parola := 0; i4Parola < len(words); i4Parola++ {
-						if i1Parola != i2Parola && i2Parola != i3Parola && i1Parola != i3Parola && i3Parola != i4Parola && i1Parola != i4Parola && i2Parola != i4Parola  {
+						if i1Parola != i2Parola && i2Parola != i3Parola && i1Parola != i3Parola && i3Parola != i4Parola && i1Parola != i4Parola && i2Parola != i4Parola {
 							response = append(response, words[i1Parola]+words[i2Parola]+words[i3Parola]+words[i4Parola])
 						}
 					}
@@ -84,7 +90,7 @@ func combina(words []string) []string {
 
 func word2Title(words []string) []string {
 
-	response := []string {}
+	response := []string{}
 
 	if len(words) > 0 {
 		for _, w := range words {
@@ -99,22 +105,31 @@ func leet(w string) string {
 	var response string
 
 	/*
-	qwertyuiopasdfghjklzxcvbnm
-	qw3r7yu10p45df6hjklzxcv8nm
+		qwertyuiopasdfghjklzxcvbnm
+		qw3r7yu10p45df6hjklzxcv8nm
 	*/
 
 	if len(w) > 0 {
 		for _, c := range w {
 			switch c {
-				case 'e', 'E': c = '3'
-				case 'i', 'I': c = '1'
-				case 'o', 'O': c = '0'
-				case 'a', 'A': c = '4'
-				case 's', 'S': c = '5'
-				case 'g', 'G': c = '6'
-				case 'b', 'B': c = '8'
-				case 'z', 'Z': c = '2'
-				case 't', 'T': c = '7'
+			case 'e', 'E':
+				c = '3'
+			case 'i', 'I':
+				c = '1'
+			case 'o', 'O':
+				c = '0'
+			case 'a', 'A':
+				c = '4'
+			case 's', 'S':
+				c = '5'
+			case 'g', 'G':
+				c = '6'
+			case 'b', 'B':
+				c = '8'
+			case 'z', 'Z':
+				c = '2'
+			case 't', 'T':
+				c = '7'
 			}
 			response += string(c)
 		}
@@ -125,7 +140,7 @@ func leet(w string) string {
 
 func leets(words []string) []string {
 
-	response := []string {}
+	response := []string{}
 
 	if len(words) > 0 {
 		for _, w := range words {
@@ -135,7 +150,6 @@ func leets(words []string) []string {
 
 	return response
 }
-
 
 func main() {
 	var words = []string{}
@@ -148,17 +162,17 @@ func main() {
 	fmt.Println("| |\\/| |/ _` | |/ / _ \\ |  | | |/ __| __|")
 	fmt.Println("| |  | | (_| |   <  __/ |__| | | (__| |_ ")
 	fmt.Println("|_|  |_|\\__,_|_|\\_\\___|_____/|_|\\___|\\__|")
-	fmt.Println("\n")							   
+	fmt.Println("\n")
 
-  	for c:=0; c<5; c++ {
-    	fmt.Print("Insert word (empty to calc)-> ")
-    	text, _ := reader.ReadString('\n')
-    	// convert CRLF to LF
-    	text = strings.Replace(text, "\n", "", -1)
-		
-    	if strings.Compare("", text) == 0 {
+	for c := 0; c < 5; c++ {
+		fmt.Print("Insert word (empty to calc)-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("", text) == 0 {
 			break
-		} else {		
+		} else {
 			words = append(words, strings.ToLower(text))
 		}
 	}
@@ -185,6 +199,20 @@ func main() {
 		passwords = append(passwords, leets(passwords)...)
 	}
 
+	// PREPARA FILE
+	fmt.Print("Output filename (default 'output.txt')-> ")
+	filename, _ := reader.ReadString('\n')
+	filename = strings.Replace(filename, "\n", "", -1)
+
+	if strings.Compare("", filename) == 0 {
+		filename = "output.txt"
+	}
+	f, err := os.Create(filename)
+	check(err)
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+
 	// VISUALIZZA
 	if len(passwords) > 0 {
 		for _, p := range passwords {
@@ -192,10 +220,14 @@ func main() {
 			h := md5.New()
 			io.WriteString(h, p)
 
-			fmt.Printf("%s, %x\n", p, h.Sum(nil))
+			str := fmt.Sprintf("%s, %x\n", p, h.Sum(nil))
+
+			_, err := w.WriteString(str)
+			check(err)
 		}
 	} else {
 		fmt.Println("Words list is empty")
 	}
-}
 
+	w.Flush()
+}
